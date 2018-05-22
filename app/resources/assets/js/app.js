2f2fdfaf8e7c9fce20c139d5a9989e3932ede316
denisov1985 <denisov1985@gmail.com>
2018-05-22 23:30:03 +0300

    Webpack config

--- app/resources/assets/js/app.js
+++ app/resources/assets/js/app.js
@@ -1,22 +1,14 @@
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import ExampleComponent from './components/ExampleComponent';
 
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+Vue.use(Buefy);
 
-require('./bootstrap');
-
-window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-console.log('ololo');
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+        ExampleComponent,
+    }
 });
--- app/resources/assets/js/components/ExampleComponent.vue
+++ app/resources/assets/js/components/ExampleComponent.vue
@@ -1,17 +1,26 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">
+                Hello World
+            </h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+            <section>
+                <b-dropdown hoverable>
+                    <button class="button is-info" slot="trigger">
+                        <span>Hover me!</span>
+                        <b-icon icon="menu-down"></b-icon>
+                    </button>
 
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+                    <b-dropdown-item>Action</b-dropdown-item>
+                    <b-dropdown-item>Another action</b-dropdown-item>
+                    <b-dropdown-item>Something else</b-dropdown-item>
+                </b-dropdown>
+            </section>
         </div>
-    </div>
+    </section>
 </template>
 
 <script>
